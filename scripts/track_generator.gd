3e914ef8d03b507eb3fa8e02d3601da3d30a652e
Jonas <bauer.joni@gmx.de>
2026-06-09 00:00:57 +0200

    Update track_generator.gd

--- scripts/track_generator.gd
+++ scripts/track_generator.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
-@export var segment_length: float = 22.0
-@export var road_width: float = 20.0
+@export var segment_length: float = 40.0
+@export var road_width: float = 40.0
 @export var curve_strength: float = 12.0
 @export var segments_visible: int = 20
 
